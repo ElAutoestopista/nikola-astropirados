0156df30f473031b6bda57a6447cbeb341a42dd7
El Autoestopista <elautoestopista@proyectopqmc.com>
2016-08-15 18:42:38 +0200

    Added js to hit statistics in FireStats

--- conf.py
+++ conf.py
@@ -1061,7 +1061,7 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = "<!-- FireStats --><script type='text/javascript' src='https://www.fernandezcordero.net:443/statistics/js/fs.js.php?site_id=3'></script>"
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
